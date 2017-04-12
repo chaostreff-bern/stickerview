72184f2860871523207debcf219bbffd28fc38a2
Tobias Rueetschi <tr@brief.li>
2017-04-12 17:08:54 +0200

    api_v1: add model Sticker

--- api_v1/models.py
+++ api_v1/models.py
@@ -8,3 +8,12 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Sticker(models.Model):
+    name    = models.CharField(max_length=50)
+    image   = models.ImageField(upload_to='stickers')
+    tags    = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.name
