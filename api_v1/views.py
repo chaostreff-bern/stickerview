2582890b4124dc2aa82dbfd6bb4ccf150a1cc031
Tobias Rueetschi <tr@brief.li>
2017-04-12 17:14:29 +0200

    api_v1: add TagViewSet

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from api_v1 import models, serializers
+
+
+class TagViewSet(ReadOnlyModelViewSet):
+    queryset            = models.Tag.objects.all()
+    serializer_class    = serializers.TagSerializer
