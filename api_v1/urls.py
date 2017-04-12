e8f8296f7530cc2d53523aaaafe08aa2851de979
Tobias Rueetschi <tr@brief.li>
2017-04-12 17:18:38 +0200

    api_v1: add url router

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import url
-from page import views
+from django.conf import settings
+from rest_framework.routers import SimpleRouter
+from api_v1 import views
 
+
+r = SimpleRouter(trailing_slash=settings.APPEND_SLASH)
+
+r.register(r'tags',     views.TagViewSet,       'tags')
+r.register(r'stickers', views.StickerViewSet,   'stickers')
 
 urlpatterns = [
 ]
+
+urlpatterns.extend(r.urls)
