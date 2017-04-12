25c117c7e5726194eb1e07017d8535aa8e7124bc
Tobias Rueetschi <tr@brief.li>
2017-04-12 17:05:14 +0200

    app api_v1 added

---
+++ api_v1/apps.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig
+
+
+class ApiV1Config(AppConfig):
+    name = 'api_v1'
---
+++ api_v1/urls.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+from page import views
+
+
+urlpatterns = [
+]
