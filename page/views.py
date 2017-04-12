0067728fa95c5b2fa8dec95fda5c2358a7541473
Tobias Rueetschi <tr@brief.li>
2017-04-12 19:56:14 +0200

    page: pass stickers and tags data to template stickers

--- page/views.py
+++ page/views.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
+from api_v1.models import Sticker, Tag
 
 
 def index(request):
+    stickers    = Sticker.objects.all()
+    tags        = Tag.objects.all()
+    content = {
+        'stickers': stickers,
+        'tags': tags,
+    }
     return render(
         request,
         'stickers.hbs',
-        {},
+        content,
     )
